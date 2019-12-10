311266fa307b8c2c45f0a8538735c726cbbd9894
Meng, Fanqi <fanqi.meng@intel.com>
2019-12-10 15:39:35 +0800

    Fixed axis bug.

--- gquest/metrics.py
+++ gquest/metrics.py
@@ -4,5 +4,5 @@ from scipy.stats import spearmanr
 def spearmans_rho(y_true, y_pred):
     return np.array([
         spearmanr(y_true[:, i], y_pred[:, i])[0] 
-        for i in range(y_true.shape[0])
+        for i in range(y_true.shape[1])
     ]).mean()
